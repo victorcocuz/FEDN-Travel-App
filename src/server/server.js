434b7e4b2b4ccdef01e2c4df7343e5da3d8fbef9
Vitor Cocuz <victor.cocuz@yahoo.com>
2020-05-20 12:44:34 +0100

    feat: configure express server and environment variables

---
+++ src/server/server.js
@@ -0,0 +1,25 @@
+// Config Dotenv to receive environment variables
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Setup Express
+const express = require('express');
+const app = express();
+
+// Use Express to create a proxy server
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
+// Setup Body Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Initialize the main project folder
+app.use(express.static('../client'))
+
+// Create express server
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => console.log(`listening on ${PORT}`));
